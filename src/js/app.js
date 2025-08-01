448e857dfbf18faf4fe602efa31d0d6cbaad55ae
katy.trop <etropkina@yandex.ru>
2025-08-01 12:14:45 +0300

    add template gulp

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+import './modules/ibg.js';
+import './modules/isWebp.js';
+//import './modules/burger.js';
+//import './modules/functions.js';
+//import './modules/spollers.js';
+//import './modules/sliders.js';
+//import './modules/dynamic-adapt.js';
